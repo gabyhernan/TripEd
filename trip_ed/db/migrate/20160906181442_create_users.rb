4f5a1a90847c5742b3f49fd4cfe6747ac2ecedea
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-06 14:16:58 -0400

    initial commit

---
+++ trip_ed/db/migrate/20160906181442_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_hash
+      t.string :school
+      t.string :school_address
+
+      t.timestamps
+    end
+  end
+end
