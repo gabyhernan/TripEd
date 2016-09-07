5c346da9aaee368da3de9242b3655377daa08d76
Gabriella Hernandez <gabyhernandezlebron@gmail.com>
2016-09-07 16:38:47 -0400

    fixes merge conflicts

--- trip_ed/app/controllers/reservations_controller.rb
+++
@@ -1,6 +0,0 @@
-class ReservationsController < ApplicationController
-  def index
-    @locations = Location.all
-    @trips = Trip.all
-  end
-end
--- trip_ed/app/models/reservation.rb
+++
@@ -1,4 +0,0 @@
-class Reservation < ApplicationRecord
-  has_many :locations
-  has_many :trips
-end
