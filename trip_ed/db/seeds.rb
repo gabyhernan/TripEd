bcfa8a6864216be860f7fe0976826859efe1c602
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-06 23:30:21 -0400

    add faker to gem file and seeed db

--- trip_ed/db/seeds.rb
+++ trip_ed/db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+
 User.delete_all
 Location.delete_all
 Trip.delete_all
