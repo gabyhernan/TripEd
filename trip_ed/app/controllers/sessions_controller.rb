b92f01454155eeb595378274a174b5a05122177b
Young Woo <ywoo@umich.edu>
2016-09-07 14:00:16 -0400

    update location and session controllers with pathing and instances

--- trip_ed/app/controllers/sessions_controller.rb
+++ trip_ed/app/controllers/sessions_controller.rb
@@ -2,17 +2,24 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(params[:email])
+    location = Location.find_by(params[:email])
 
     if user
       sessions[:user_id] = user.id
+      session[:type] = 'user'
+      redirect_to '/'
+    elsif location
+      sessions[:user_id] = location.id
+      session[:type] = 'location'
       redirect_to '/'
     else
-      redirect_to 'users/new'
+      redirect_to '/users/new'
     end
   end
 
   def destroy
     session[:user_id] = nil
+    session[:type] = nil
     redirect_to '/'
   end
 
