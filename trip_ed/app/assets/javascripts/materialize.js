c5645da9f5ce9bc123b204335367cee493f10793
Gaby Hernandez <gabyhernandezlebron@gmail.com>
2016-09-10 14:32:34 -0400

    adds materialize and fixes

---
+++ trip_ed/app/assets/javascripts/materialize.js
@@ -0,0 +1,11 @@
+// this is an example of how turbolinks using materialize should be set up
+// there was a gem created to get this to work and you have to call these events in a
+// specific syntax below
+
+// link below has a more examples
+// https://github.com/kossnocorp/jquery.turbolinks
+
+
+$(document).ready(function() {
+  $('select').material_select();
+});
