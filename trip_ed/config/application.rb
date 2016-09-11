ea0728dd5faa35d57eb6c25a8c51374bd0dce195
Stefan Hartmann <stefanhartmann@gmail.com>
2016-09-10 21:35:15 -0400

    enable redis cacheing

--- trip_ed/config/application.rb
+++ trip_ed/config/application.rb
@@ -12,8 +12,7 @@ Bundler.require(*Rails.groups)
 
 module TripEd
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.active_record.raise_in_transactional_callbacks = true
+    config.cache_store = :redis_store, 'redis://localhost:6379/0/cache', { expires_in: 90.minutes }
   end
 end
